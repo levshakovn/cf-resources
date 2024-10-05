dfb03187b9267825cec56b34e3352b1c3f749eba
Nikolai Levshakov <levshakovn@gmail.com>
2024-10-05 20:24:42 +0200

    Updated scripts

--- common/s3.sh
+++ common/s3.sh
@@ -6,7 +6,7 @@ upload_file_to_bucket () {
     aws s3api put-object \
         --bucket $_BUCKET_NAME \
         --key $_FILE_NAME \
-        --body $_FILE_NAME \
+        --body $_FILE_PATH \
         --content-type "text/html" \
         --region "$AWS_REGION" > /dev/null
 }
@@ -19,12 +19,13 @@ get_bucket_status () {
 clear_bucket () {
     _BUCKET_NAME=$1
     _BUCKET_STATUS=$(get_bucket_status $_BUCKET_NAME)
-    _OBJECT_VERSIONS=$(aws s3api list-object-versions --bucket "$_BUCKET_NAME" | jq -r --arg key "key-value" '{Objects: [.Versions[] | {Key, VersionId}], Quiet: false}')
+    _BUCKET_OBJECTS=$(aws s3api list-object-versions --bucket "$_BUCKET_NAME")
     if echo "$BUCKET_STATUS" | grep -q 'Not Found'; then
         echo "$(date +%H:%M:%S) -- Bucket $_BUCKET_NAME is already deleted."
-    elif [ -z "$_OBJECT_VERSIONS" ]; then
+    elif [ -z "$_BUCKET_OBJECTS" ]; then
         echo "$(date +%H:%M:%S) -- Bucket $_BUCKET_NAME is already empty."
     else
+        _OBJECT_VERSIONS=$(aws s3api list-object-versions --bucket "$_BUCKET_NAME" | jq -r --arg key "key-value" '{Objects: [.Versions[] | {Key, VersionId}], Quiet: false}')
         echo "Emptying the bucket $_BUCKET_NAME..."
         # Delete all objects and versions in the bucket
         aws s3api delete-objects \
